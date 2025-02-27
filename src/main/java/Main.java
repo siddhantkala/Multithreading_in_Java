527ff4a7391fba4ff19b63587c8308fed97dbc19
Siddhant Kala <Siddhant03Kala@gmail.com>
2025-02-27 08:03:57 +0530

    Create thread through implementing Runnable interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main method started");
-        System.out.println(Thread.currentThread().getName());
         World world = new World();
-        world.start();
+        Thread t = new Thread(world);
+        t.start();
 
         for(;;)
         {
--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,4 +1,4 @@
-public class World extends Thread{
+public class World implements Runnable{
     @Override
     public void run() {
         for(;;)
