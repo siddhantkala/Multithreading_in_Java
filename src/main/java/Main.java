689e46d4fc9447c2fa63959e630759fefae5f9ca
Siddhant Kala <Siddhant03Kala@gmail.com>
2025-02-27 07:59:31 +0530

    Create thread through extending Thread class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main method started");
         System.out.println(Thread.currentThread().getName());
+        World world = new World();
+        world.start();
+
+        for(;;)
+        {
+            System.out.println(Thread.currentThread().getName());
+        }
     }
 }
---
+++ src/main/java/World.java
@@ -0,0 +1,9 @@
+public class World extends Thread{
+    @Override
+    public void run() {
+        for(;;)
+        {
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
