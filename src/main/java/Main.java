dec66031a3a98f06a07ab383a4ae640bc7ec37a1
Siddhant Kala <Siddhant03Kala@gmail.com>
2025-02-27 07:37:22 +0530

    To get thread of main method

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main method started");
+        System.out.println(Thread.currentThread().getName());
+    }
+}
