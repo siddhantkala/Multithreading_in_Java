9264e7a88be6f089a17d093d56fc0bb0cb9d36f9
Siddhant Kala <Siddhant03Kala@gmail.com>
2025-03-02 17:23:51 +0530

    Implemented Locks with Reentrantlock

--- src/main/java/Locks/BankAccount.java
+++ src/main/java/Locks/BankAccount.java
@@ -1,25 +1,39 @@
 package Locks;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private int balance = 100;
+    private final Lock lock = new ReentrantLock();
 
-    public synchronized void withdraw(int amount)
+    public void withdraw(int amount)
     {
         System.out.println(Thread.currentThread().getName()+" attempting to withdraw "+amount);
-        if(balance>=amount)
+        if(lock.tryLock())
         {
-            System.out.println(Thread.currentThread().getName()+" proceeding with withdrawal");
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            System.out.println(Thread.currentThread().getName()+" acquired lock");
+            if(balance>=amount)
+            {
+                try {
+                    System.out.println(Thread.currentThread().getName()+" proceeding with withdrawal");
+                    Thread.sleep(10000);
+                    balance-=amount;
+                    System.out.println(Thread.currentThread().getName()+" completed withdrawal. Remaining balance is "+balance);
+                } catch (InterruptedException e) {
+                }
+                finally {
+                    lock.unlock();
+                }
             }
-            balance-=amount;
-            System.out.println(Thread.currentThread().getName()+" completed withdrawal. Remaining balance is "+balance);
+            else
+            {
+                System.out.println(Thread.currentThread().getName()+" insufficient balance");
+            }
         }
         else
         {
-            System.out.println(Thread.currentThread().getName()+" insufficient balance");
+            System.out.println(Thread.currentThread().getName()+" Couldn't acquire lock, will try later");
         }
     }
 
